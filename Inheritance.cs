cb002029fa1bceb46c111e7f23343af69666f0a7
Mert Topcu <merttopcu.dev@gmail.com>
2024-08-02 21:22:03 +0300

    Add project files.

---
+++ Inheritance.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PatikaInheritanceTask
+{
+    //▼ Base Class ▼
+    public class BasePerson
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Name:{Name}\nSurname:{Surname}");
+        }
+
+    }
+
+    // ▼ Student class inheritance from BasePerson with additional Student number prop ▼
+    public class Student:BasePerson
+    {
+        public int StudentNumber { get; set; }
+        // ▼ Ctor
+        public Student(string name, string surname, int studentNumber)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.StudentNumber = studentNumber;
+        }
+        // ▼ For displaying infos of student
+        public void DisplayInfo()
+        {
+            //Using inheritance BasePerson method
+            Greeting();
+
+            Console.WriteLine($"Student Number:{StudentNumber}");
+        }
+    }
+
+    // ▼ Teacher class inheritance from BasePerson with additional salary prop ▼
+    public class Teacher:BasePerson
+    {
+        public double Salary { get; set; }
+
+        public Teacher(string name, string surname, int salary)
+        {
+            this.Name=name;
+            this.Surname = surname;
+            this.Salary=salary;
+        }
+        // ▼ For displaying infos of teacher
+        public void DisplayInfo()
+        {
+            //Using inheritance BasePerson method
+            Greeting();
+
+            Console.WriteLine($"Salary:{Salary}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaInheritanceTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ▼ Instance for teacher and defining with ctor ▼
+            Teacher teacher=new Teacher("Sertan","Bozkuş",60000);
+            // ▼ Showing infos of teacher ▼
+            teacher.DisplayInfo();
+
+            // ▼ Instance for student and defining with ctor ▼
+            Student student =new Student("Mert","Topcu",3);
+            // ▼ Showing infos of student ▼
+            student.DisplayInfo();
+        }
+    }
+}
